3aa1dd6f4d94185b33d89959957827ef672343ca
John Doe <johndoe@johndoe.com>
2021-12-30 20:57:17 -0500

    update SQL code

--- sql_test_data.sql
+++ sql_test_data.sql
@@ -1,9 +1,32 @@
 /* create table */
 DROP TABLE IF EXISTS accounts;
 
+DROP TABLE IF EXISTS rooms;
+
+DROP TABLE IF EXISTS chats;
+
 CREATE TABLE accounts (
     id serial PRIMARY KEY,
-    email varchar(30) NOT NULL UNIQUE,
+    username varchar(16) NOT NULL UNIQUE,
     passhash varchar(60) NOT NULL,
-    verified BOOLEAN NOT NULL DEFAULT FALSE
+    verified boolean NOT NULL DEFAULT FALSE,
+    room_id int DEFAULT NULL
 );
+
+CREATE TABLE rooms (
+    id serial PRIMARY KEY,
+    roomname varchar(16) NOT NULL UNIQUE,
+    passhash varchar(60) NOT NULL,
+    protected boolean NOT NULL DEFAULT FALSE,
+    capacity int NOT NULL,
+    active boolean NOT NULL DEFAULT TRUE
+);
+
+CREATE TABLE chats (
+    id serial PRIMARY KEY,
+    account_id int NOT NULL,
+    room_id int NOT NULL,
+    content varchar(200) NOT NULL,
+    sendtime timestamp NOT NULL
+);
+
